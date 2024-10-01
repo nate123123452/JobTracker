430c5e9299bf37f25b0d286061a3e254e5d0cdae
Nathan <nathat3@uci.edu>
2024-10-01 11:59:57 -0700

    added extracting attributes

--- scrape_jobs/models.py
+++ scrape_jobs/models.py
@@ -3,11 +3,9 @@ from django.db import models
 class JobListing(models.Model):
     title = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    description = models.TextField()
     date_posted = models.DateField()
-    salary = models.CharField(max_length=100)
-    company = models.CharField(max_length=255, default = "N/A")  # New field for company name
-    
+    company = models.CharField(max_length=255, default="N/A")  # New field for company name
+    attributes = models.TextField(blank=True, default="N/A")  # New field for job attributes
 
     def __str__(self):
         return self.title
