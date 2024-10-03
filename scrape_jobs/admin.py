b8aa17079efa05dacc2a3caf5af4e1f9415bcede
Nathan <nathat3@uci.edu>
2024-10-02 17:31:41 -0700

    adjusted list display

--- scrape_jobs/admin.py
+++ scrape_jobs/admin.py
@@ -3,13 +3,13 @@ from .models import JobListing
 
 class JobListingAdmin(admin.ModelAdmin):
     # Define the fields to display in the admin list view
-    list_display = ('title', 'company', 'location', 'attributes', 'date_posted')
+    list_display = ('title', 'company', 'location', 'attributes', 'date_posted', 'job_description')
 
     # Add search fields to search for jobs based on these fields
-    search_fields = ('title', 'company', 'location', 'attributes', 'date_posted')
+    search_fields = ('title', 'company', 'location', 'attributes', 'date_posted', 'job_description')
 
     # Add filters for quick filtering by company and date posted
-    list_filters = ('title', 'company', 'location', 'attributes', 'date_posted')
+    list_filters = ('title', 'company', 'location', 'attributes', 'date_posted', 'job_description')
 
 # Register the model with the admin site
 admin.site.register(JobListing, JobListingAdmin)
