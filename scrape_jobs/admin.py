413a547dc64da0f3197200c1565092cccdc3468a
Nathan <nathat3@uci.edu>
2024-10-26 11:55:51 -0700

    changed courses. no longer indeed scraper but user has to manually input job description

--- scrape_jobs/admin.py
+++ scrape_jobs/admin.py
@@ -3,21 +3,21 @@ from .models import JobListing, ResumeListing, Keyword
 
 # JobListing Admin
 class JobListingAdmin(admin.ModelAdmin):
-    list_display = ('title', 'company', 'location', 'attributes', 'date_posted', 'job_description')
-    search_fields = ('title', 'company', 'location', 'attributes', 'date_posted', 'job_description')
-    list_filter = ('title', 'company', 'location', 'date_posted')  # Removed job_description and attributes from filter
+    list_display = ('job_name', 'website', 'text',)  # Display fields for JobListing
+    search_fields = ('job_name', 'website', 'text',)  # Allow searching by these fields
+    list_filter = ('job_name', 'website', 'keywords')  # Filter by job name, website, and keywords
 
 # ResumeListing Admin
 class ResumeListingAdmin(admin.ModelAdmin):
-    list_display = ('text',)
-    search_fields = ('text',)
-    list_filter = ('keywords',)  # Assuming you want to filter by keywords
+    list_display = ('text',)  # Display the text field
+    search_fields = ('text',)  # Allow searching by text
+    list_filter = ('keywords',)  # Allow filtering by keywords
 
 # Keyword Admin
 class KeywordAdmin(admin.ModelAdmin):
-    list_display = ('words',)
-    search_fields = ('words',)
-    list_filter = ('words',)  # You can filter by keywords
+    list_display = ('words',)  # Display the keyword words
+    search_fields = ('words',)  # Allow searching by words
+    list_filter = ('words',)  # Filter by keywords
 
 # Register the models with the admin site
 admin.site.register(JobListing, JobListingAdmin)
--- scrape_jobs/models.py
+++ scrape_jobs/models.py
@@ -7,18 +7,13 @@ class Keyword(models.Model):
         return self.words
 
 class JobListing(models.Model):
-    title = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
-    company = models.CharField(max_length=255)
-    date_posted = models.DateField()
-    attributes = models.TextField()
-    benefits = models.TextField(default = 'N/A')  # Add this field if it doesn't exist
-    job_description = models.TextField( default = 'N/A')  # Ensure this field is present
-    keywords = models.ManyToManyField(Keyword, related_name='job_listings')
-
+    job_name = models.CharField(max_length=255, default = 'N/A')
+    website = models.URLField( default = 'N/A')
+    text = models.TextField( default = 'N/A')
+    keywords = models.ManyToManyField(Keyword, related_name='job_listings')   
     def __str__(self):
-        return self.title
-    
+        return self.job_name
+
 class ResumeListing(models.Model):
     text = models.TextField( default = 'N/A')
     keywords = models.ManyToManyField(Keyword, related_name='resume_listings')
