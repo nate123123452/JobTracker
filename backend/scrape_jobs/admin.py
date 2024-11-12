29d94d4e182c47d292dbe27a9de2046fbe312830
Nathan <nathat3@uci.edu>
2024-11-11 20:13:02 -0800

    fixed models

--- backend/scrape_jobs/admin.py
+++ backend/scrape_jobs/admin.py
@@ -1,26 +1,20 @@
 from django.contrib import admin
-from .models import JobListing, ResumeListing, Job  # Import Job model
+from .models import Resume, Job
 
-# JobListing Admin
-class JobListingAdmin(admin.ModelAdmin):
-    list_display = ('job_name', 'website', 'text',)
-    search_fields = ('job_name', 'website', 'text',)
-    list_filter = ('job_name', 'website', 'keywords')
-
-# ResumeListing Admin
-class ResumeListingAdmin(admin.ModelAdmin):
-    list_display = ('text',)
-    search_fields = ('text',)
-    list_filter = ('keywords',)
+# Resume Admin
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'upload_date', 'description', 'document')  # Columns to display
+    search_fields = ('title', 'description')  # Fields to search by
+    list_filter = ('upload_date',)  # Add filters on the right sidebar
+    ordering = ('-upload_date',)  # Order by upload date, newest first
 
 # Job Admin
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('company', 'title', 'status', 'location', 'applied_date')  # Display relevant fields
-    search_fields = ('company', 'title', 'status')  # Allow searching by company, title, status
-    list_filter = ('status', 'location')  # Filter by status and location
-    ordering = ('-applied_date',)  # Sort by applied date, newest first
+    list_display = ('company', 'title', 'status', 'location', 'applied_date')  # Columns to display
+    search_fields = ('company', 'title', 'status')  # Fields to search by
+    list_filter = ('status', 'location')  # Add filters on the right sidebar
+    ordering = ('-applied_date',)  # Order by applied date, newest first
 
-# Register the models with the admin site
-admin.site.register(JobListing, JobListingAdmin)
-admin.site.register(ResumeListing, ResumeListingAdmin)
-admin.site.register(Job, JobAdmin)  # Register the Job model
+# Register models to admin
+admin.site.register(Resume, ResumeAdmin)
+admin.site.register(Job, JobAdmin)
--- backend/scrape_jobs/views.py
+++ backend/scrape_jobs/views.py
@@ -2,6 +2,18 @@ from rest_framework import viewsets
 from .models import Job
 from .serializers import JobSerializer
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.utils import timezone
+from .models import Resume
+from 
+
+
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+@api_view(['POST'])
+def upload_resume(request):
+    file = request
