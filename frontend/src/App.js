8812adbc0d874b503609c1c83c8ee0716038a8b9
Nathan Tran <nathantran@Nathans-MacBook-Pro.local>
2024-11-25 14:17:39 -0800

    fixed navbar functionality and toast messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,63 @@ import HomePage from './pages/home';
 import JobPage from './pages/job';
 import ResumePage from './pages/resume';
 import CalendarPage from './pages/calendar';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Modal from './components/Modal';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const [isModelOpen, setIsModelOpen] = React.useState(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [username, setUsername] = React.useState('');
+
+
+  const toggleModal = () => setIsModelOpen(!isModelOpen);
+
+  const handleLoginSuccess = (username) => {
+    setIsLoggedIn(true);
+    setUsername(username);
+    localStorage.setItem('isLoggedIn', 'true');
+    localStorage.setItem('username', username);
+    toggleModal();
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem('username');
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    toast.success('Logged out successfully', {
+      position: 'top-center',
+      autoClose: 1500,
+    });
+  }
+  
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
   return (
     <Router>
-      <Navbar />
+      <Navbar 
+        isLoggedIn={isLoggedIn} 
+        username={username} 
+        handleLogout={handleLogout} 
+        toggleModal={toggleModal}
+        setIsLoggedIn={setIsLoggedIn}
+        setUsername={setUsername}
+      />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage onGetStartedClick={toggleModal} isLoggedIn={isLoggedIn}/>} />
         <Route path="/jobs" element={<JobPage />} />
         <Route path="/resumes" element={<ResumePage />} />
         <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
+      <Modal isOpen={isModelOpen} onClose={toggleModal}>
+        <LoginForm onLoginSuccess={handleLoginSuccess} />
+      </Modal>
       <ToastContainer position="top-center" autoClose={1500} />
     </Router>
   );
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,126 +3,156 @@ import { ReactTyped as Typed } from 'react-typed';
 import jobImage from '../assets/job.jpg';
 import resumeImage from '../assets/resume.png';
 import calendarImage from '../assets/calendar.jpg';
-import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom';  
-import Modal from '../components/Modal';
-import LoginForm from '../components/LoginForm';
+import { motion, useInView } from 'framer-motion';
+import { Link, useNavigate } from 'react-router-dom';
 
-const HomePage = () => {
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
+const HomePage = ({ isLoggedIn, onGetStartedClick }) => {
+  const navigate = useNavigate();
 
-  const toggleModal = () => setIsModalOpen(!isModalOpen);
+  const handleGetStartedClick = () => {
+    navigate('/');
+    onGetStartedClick();
+  };
 
-  const handleLoginSuccess = () => {
-    toggleModal();
-  }
+
+  // Refs and visibility states for each section
+  const jobRef = React.useRef(null);
+  const isJobInView = useInView(jobRef, { once: true });
+
+  const resumeRef = React.useRef(null);
+  const isResumeInView = useInView(resumeRef, { once: true });
+
+  const calendarRef = React.useRef(null);
+  const isCalendarInView = useInView(calendarRef, { once: true });  
+  
 
   return (
-    <motion.div 
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 1, ease: "easeInOut" }}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
     >
       {/* Hero Section */}
-      <div className='text-white max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <p className='text-[#00df9a] font-bold p-2 text-xl'>Land Your Dream Job</p>
-        <h1 className='md:text-6xl sm:text-5xl text-4xl font-bold md:py-6'>Manage Your Career</h1>
-        <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-3'>Keep Your Job Searches</p>
-          <Typed 
-            className='md:text-5xl sm:text-4xl text-xl font-bold pl-1.5'
-            strings={['Organized', 'Simplified', 'Optimized']} 
-            typeSpeed={100} 
-            backSpeed={100} 
+      <div className="text-white max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2 text-xl">Land Your Dream Job</p>
+        <h1 className="md:text-6xl sm:text-5xl text-4xl font-bold md:py-6">Manage Your Career</h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-3">Keep Your Job Searches</p>
+          <Typed
+            className="md:text-5xl sm:text-4xl text-xl font-bold pl-1.5"
+            strings={['Organized', 'Simplified', 'Optimized']}
+            typeSpeed={100}
+            backSpeed={100}
             loop
           />
         </div>
-        <p className='md:text-2xl text-xl font-bold text-gray-500'>Track, schedule, and manage your job search with ease</p>
-        <button onClick={toggleModal} className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">Track, schedule, and manage your job search with ease</p>
+        {!isLoggedIn && (
+          <button
+            onClick={handleGetStartedClick}
+            className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black"
+          >
+            Get Started
+          </button>
+        )}
       </div>
 
       {/* Jobs Section */}
-      <div className='w-full bg-gray-50 py-16 px-4 border-t border-gray-400'>
-        <div className='max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center'>
+      <div ref={jobRef} className="w-full bg-gray-50 py-16 px-4 border-t border-gray-400">
+        <motion.div
+          className="max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center"
+          initial={{ opacity: 0, x: -100 }}
+          animate={isJobInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1, ease: 'easeOut', delay: 0.25}}
+        >
           <img
             src={jobImage}
-            alt='job-dashboard'
-            className='w-full h-auto max-w-[600px] md:max-w-[700px] mx-auto mb-4 md:mb-0 rounded-lg shadow-md transition-transform duration-300 hover:scale-105 order-1 md:order-1'
+            alt="job-dashboard"
+            className="w-full h-auto max-w-[600px] md:max-w-[700px] mx-auto mb-4 md:mb-0 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
           />
-          <div className='flex flex-col justify-center order-2 md:order-2'>
-            <p className='text-[#00df9a] font-bold'>JOB DASHBOARD</p>
-            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Track Your Job Applications</h1>
-            <p className='text-lg text-gray-700 mb-2'>
+          <div className="flex flex-col justify-center">
+            <p className="text-[#00df9a] font-bold">JOB DASHBOARD</p>
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Track Your Job Applications</h1>
+            <p className="text-lg text-gray-700 mb-2">
               Stay organized in your job search and manage your applications effectively.
             </p>
-            <p className='text-lg text-gray-700'>
-              Our platform allows you to track multiple applications, schedule interviews, and ensure you never miss an opportunity.
+            <p className="text-lg text-gray-700">
+              Our platform allows you to track multiple applications, schedule interviews, and ensure you never miss an
+              opportunity.
             </p>
-            <Link to='/jobs'>
-              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>
+            <Link to="/jobs">
+              <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
                 Add Jobs
               </button>
             </Link>
           </div>
-        </div>
+        </motion.div>
       </div>
 
       {/* Resumes Section */}
-      <div className='w-full bg-gray-50 py-16 px-4 border-t border-gray-400'>
-        <div className='max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center'>
-          <div className='flex flex-col justify-center order-2 md:order-1'>
-            <p className='text-[#00df9a] font-bold'>RESUME DASHBOARD</p>
-            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Organize and Track Your Resumes</h1>
-            <p className='text-lg text-gray-700 mb-2'>
+      <div ref={resumeRef} className="w-full bg-gray-50 py-16 px-4 border-t border-gray-400">
+        <motion.div
+          className="max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center"
+          initial={{ opacity: 0, x: 100 }}
+          animate={isResumeInView ? { opacity: 1, x: 0 } : {}}
+          transition={{ duration: 1, ease: 'easeInOut', delay: 0.25 }}
+        >
+          <img
+            src={resumeImage}
+            alt="resume"
+            className="w-full h-auto max-w-[300px] md:max-w-[400px] mx-auto mb-4 md:mb-0 rounded-lg shadow-md transition-transform duration-300 hover:scale-105 order-1 md:order-2"
+          />
+          <div className="flex flex-col justify-center order-2 md:order-1">
+            <p className="text-[#00df9a] font-bold">RESUME DASHBOARD</p>
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Organize and Track Your Resumes</h1>
+            <p className="text-lg text-gray-700 mb-2">
               Keep all your resumes in one place for easy access and management.
             </p>
-            <p className='text-lg text-gray-700'>
+            <p className="text-lg text-gray-700">
               Upload, update, and monitor your resumes to stay prepared for job applications at any time.
             </p>
-            <Link to='/resumes'>
-              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>
+            <Link to="/resumes">
+              <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
                 Upload Resume
               </button>
             </Link>
           </div>
-          <img
-            src={resumeImage}
-            alt='resume'
-            className='w-full h-auto max-w-[300px] md:max-w-[400px] mx-auto mb-4 md:mb-0 rounded-lg shadow-md transition-transform duration-300 hover:scale-105 order-1 md:order-2'
-          />
-        </div>
+          
+          </motion.div>
       </div>
 
       {/* Calendar Section */}
-      <div className='w-full bg-gray-50 py-16 px-4 border-t border-gray-400'>
-        <div className='max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center'>
+      <div ref={calendarRef} className="w-full bg-gray-50 py-16 px-4 border-t border-gray-400">
+        <motion.div
+          className="max-w-[1240px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center"
+          initial={{ opacity: 0, y: 100 }}
+          animate={isCalendarInView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 1, ease: 'easeInOut', delay: 0.25 }}
+        >
           <img
             src={calendarImage}
-            alt='calendar-dashboard'
-            className='w-full h-auto max-w-[400px] mx-auto mb-4 md:mb-0 rounded-lg shadow-md transition-transform duration-300 hover:scale-105 order-1 md:order-1'
+            alt="calendar-dashboard"
+            className="w-full h-auto max-w-[400px] mx-auto mb-4 md:mb-0 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
           />
-          <div className='flex flex-col justify-center order-2 md:order-2'>
-            <p className='text-[#00df9a] font-bold'>CALENDAR DASHBOARD</p>
-            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Your Job Search Schedule</h1>
-            <p className='text-lg text-gray-700 mb-2'>
+          <div className="flex flex-col justify-center">
+            <p className="text-[#00df9a] font-bold">CALENDAR DASHBOARD</p>
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Manage Your Job Search Schedule</h1>
+            <p className="text-lg text-gray-700 mb-2">
               Keep track of your upcoming interviews, deadlines, and follow-ups.
             </p>
-            <p className='text-lg text-gray-700'>
-              Our Calendar Dashboard integrates all your job search activities so you can stay on top of your schedule and never miss an important event.
+            <p className="text-lg text-gray-700">
+              Our Calendar Dashboard integrates all your job search activities so you can stay on top of your schedule and
+              never miss an important event.
             </p>
-            <Link to='/calendar'>
-              <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>
+            <Link to="/calendar">
+              <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3">
                 View Calendar
               </button>
             </Link>
           </div>
-        </div>
+        </motion.div>
       </div>
-
-      <Modal isOpen={isModalOpen} onClose={toggleModal}>
-        <LoginForm onLoginSuccess={handleLoginSuccess} />
-      </Modal>
     </motion.div>
   );
 };
